7a83875e4918fdb627410b06079737cb66d53b26
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-08-12 15:00:52 -0600

    making redux containers and components

---
+++ src/components/MessagesList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Message from './Messages'
+
+const MessagesList = ({messages}) => {
+    <section>
+        <ul>
+            {messages.map(message => {
+                <Message 
+                key={message.id}
+                {...message}/>
+            })}
+        </ul>
+    </section>
+}
+
+MessagesList.PropTypes = {
+    messages: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            author: PropTypes.string.isRequired,
+            message: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired
+}
+
+export default MessagesList
