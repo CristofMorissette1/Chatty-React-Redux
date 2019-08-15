cbc42aac36ce166edad7e90a6a052fbb50a2add1
Cristof Morissette <cristofmorissette@Cristofs-MacBook-Air.local>
2019-08-15 14:21:29 -0600

    implenting client side socket

--- src/sockets/Index.js
+++ src/sockets/Index.js
@@ -1,5 +1,5 @@
 import * as types from '../constants/ActionTypes'
-import {addUser, messageReceiced, populateUsersList} from '../actions'
+import { addUser, messageReceiced, populateUsersList } from '../actions'
 
 const setupSocket = (dispatch, username) => {
     const socket = new WebSocket('ws://localhost:8989')
